0e381c3850d302be9793817026e67c98aff97ccf
Swapnanil Dutta <47251193+swapnanildutta@users.noreply.github.com>
2020-08-18 19:42:42 +0530

    Add Atbash Cipher

--- CiphX.py
+++ CiphX.py
@@ -3,6 +3,7 @@ from PIL import ImageTk, Image
 import random
 import Vigenere
 import PlayFair
+from CipheringAlgos.AtbashCipher import Atbashconvert
 
 root = Tk()
 root.title("CiphX")
@@ -198,6 +199,75 @@ def sel():
         btn4.configure(font=("Helvetica", 10))
         btn4.place(x=800, y=600, anchor="center")
 
+    if s == "Atbash Cipher":
+        # Atbash cipher
+
+        et = Toplevel()
+        et.title("Atbash Cipher")
+        et.iconbitmap('disneyland.ico')
+        et.configure(bg="Pink")
+        et.geometry("2000x2000")
+        et.bind('<Home>', lambda event: et.state('normal'))
+        et.bind('<F12>', lambda event: et.state('zoomed'))
+
+        def Encode():
+            txt = ent1.get()
+
+            st = Atbashconvert(txt)
+
+            global lbl
+            lbl = Label(et, text="Ciphered Message : "+st, font=("Helvetica", 25))
+            lbl.place(x=700, y=500, anchor="center")
+
+        def Decode():
+            txt = ent1.get()
+
+            st = Atbashconvert(txt)
+
+            global lbl
+            lbl = Label(et, text="Original Message : "+st, font=("Helvetica", 25))
+            lbl.place(x=700, y=500, anchor="center")
+
+        def Ref():
+            ent1.delete(0, END)
+            lbl.destroy()
+
+        def Back():
+            et.destroy()
+
+        lb = Label(et, padx=30, text="ATBASH CIPHER", font=("Helvetica", 40))
+        lb.place(x=700, y=50, anchor="center")
+
+        k1 = Label(et, padx=10, text="Press F12 : FullScreen", bg="Lightyellow", font=("Helvetica", 10))
+        k1.place(x=600, y=100, anchor="center")
+
+        k2 = Label(et, padx=10, text="Press Home : NormalScreen", bg="Lightyellow", font=("Helvetica", 10))
+        k2.place(x=800, y=100, anchor="center")
+
+        lb1 = Label(et, padx=30, text="Enter Message :", font=("Helvetica", 20))
+        lb1.place(x=700, y=150, anchor="center")
+
+        ent1 = Entry(et, width=20, background="Skyblue", borderwidth=3)
+        ent1.configure(font=("Helvetica", 20))
+        ent1.place(x=700, y=200, anchor="center")
+
+        # Create Button
+
+        btn1 = Button(et, borderwidth=0, padx=20, pady=20, command=Encode)
+        btn1.configure(font=("Helvetica", 20), image=im1, bg="Pink")
+        btn1.place(x=500, y=400, anchor="center")
+
+        btn2 = Button(et, borderwidth=0, padx=20, pady=20, command=Decode)
+        btn2.configure(font=("Helvetica", 20), image=im2, bg="Pink")
+        btn2.place(x=900, y=400, anchor="center")
+
+        btn3 = Button(et, text="REFRESH", borderwidth=0, padx=50, pady=10, command=Ref)
+        btn3.configure(font=("Helvetica", 10))
+        btn3.place(x=600, y=600, anchor="center")
+
+        btn4 = Button(et, text="BACK TO HOMESCREEN", borderwidth=0, padx=10, pady=10, command=Back)
+        btn4.configure(font=("Helvetica", 10))
+        btn4.place(x=800, y=600, anchor="center")
 
 def Close():
     root.destroy()
@@ -209,7 +279,7 @@ clicked.set("PlayFair Cipher")
 lb = Label(root, padx=10, text="Choose The Ciphering Method :", font=("Helvetica", 25), background="Pink")
 lb.place(x=480, y=300, anchor="center")
 
-dd = OptionMenu(root, clicked, "Vigenere Cipher", "PlayFair Cipher")
+dd = OptionMenu(root, clicked, "Vigenere Cipher", "PlayFair Cipher", "Atbash Cipher")
 dd.configure(font=("Helvetica", 25), bg="Skyblue", width=27)
 dd.place(x=1000, y=300, anchor="center")
 
