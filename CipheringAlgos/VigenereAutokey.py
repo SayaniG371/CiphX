7f8f4bc3c7b48edaf9bafb41efd619ff68bf3c4a
SayaniG371 <55864408+SayaniG371@users.noreply.github.com>
2020-08-20 19:26:22 +0530

    Update VigenereAutokey.py

--- CipheringAlgos/VigenereAutokey.py
+++ CipheringAlgos/VigenereAutokey.py
@@ -1,14 +1,15 @@
-chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def Enc(strn, key):
-    # Creating key upto message length
-    i = 0
-    while len(key) != len(strn):
-        key += strn[i]
-        i += 1
 
     # Encrypting process
     if strn.isupper():
+        # Creating key upto message length
+        i = 0
+        while len(key) != len(strn):
+            key += strn[i]
+            i += 1
+        # encryption
         st = ""
         for i in range(len(strn)):
             if strn[i] not in chars:
@@ -21,12 +22,18 @@ def Enc(strn, key):
     if strn.islower():
         strn = strn.upper()
         key = key.upper()
+        # Creating key upto message length
+        i = 0
+        while len(key) != len(strn):
+            key += strn[i]
+            i += 1
+        # encryption
         st = ""
         for i in range(len(strn)):
             if strn[i] not in chars:
                 st += strn[i]
                 continue
-            s = ((ord(strn[i]) + ord(key[i])) % 26) + ord('a')
+            s = ((ord(strn[i]) + ord(key[i])) % 26) + ord('A')
             st += chr(s)
         st = st.lower()
 
@@ -35,13 +42,14 @@ def Enc(strn, key):
 
 
 def Dec(strn, key):
-    # Creating key
-    i = 0
-    while len(key) != len(strn):
-        key += chr(((ord(strn[i]) - ord(key[i])) % 26) + ord('A'))
-        i += 1
-
+    # Decryption process
     if strn.isupper():
+        # Creating key
+        i = 0
+        while len(key) != len(strn):
+            key += chr(((ord(strn[i]) - ord(key[i])) % 26) + ord('A'))
+            i += 1
+        # decryption
         st = ""
         for i in range(len(strn)):
             if strn[i] not in chars:
@@ -54,6 +62,12 @@ def Dec(strn, key):
     if strn.islower():
         strn = strn.upper()
         key = key.upper()
+        # Creating key
+        i = 0
+        while len(key) != len(strn):
+            key += chr(((ord(strn[i]) - ord(key[i])) % 26) + ord('A'))
+            i += 1
+        # decryption
         st = ""
         for i in range(len(strn)):
             if strn[i] not in chars:
