92afeb4d7a8e7641d42d53c22770218e95e3fee0
Saloni Swagata <saloniswagata@gmail.com>
2020-08-20 18:35:08 +0530

    Added Caeser

---
+++ CipheringAlgos/Caeser.py
@@ -0,0 +1,21 @@
+def enc():
+    s = input("Enter the string to be encrypted: ")
+    k = input("Enter the number: ")
+    e=""
+    for letter in s:
+        if letter.isupper():
+            e+= chr((ord(letter) + k-65) % 26 + 65)
+        else:
+            e += chr((ord(letter) + k - 97) % 26 + 97)
+    print(f"Encryted String: {e}")
+
+def dec():
+    s = input("Enter the string to be decrypted: ")
+    k = input("Enter the number: ")
+    d=""
+    for letter in s:
+        if letter.isupper():
+             d+= chr((ord(letter) + (26-k)-65) % 26 + 65)
+        else:
+            d+= chr((ord(letter) + (26-k) - 97) % 26 + 97)
+    print(f"Decryted String: {d}")
