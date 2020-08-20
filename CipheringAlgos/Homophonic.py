d875e001531f710f51fb525bb91af96533213b55
SayaniG371 <55864408+SayaniG371@users.noreply.github.com>
2020-08-20 19:35:31 +0530

    Update Homophonic.py

--- CipheringAlgos/Homophonic.py
+++ CipheringAlgos/Homophonic.py
@@ -2,8 +2,7 @@ import random
 
 key_table={'A':['D','9'],'B':'X','C':'S','D':'F','E':['Z','7','2','1'],'F':'E','G':'H','H':'C','I':['V','3'],'J':'I','K':'T','L':'P','M':'G','N':['A','5'],'O':['Q','0'],'P':'L','Q':'K','R':'J','S':['R','4'],'T':['U','6'],'U':'O','V':'W','W':'M','X':'Y','Y':'B','Z':'N'}
 
-def encrypt():
-    msg = input("Enter the message to be encrypted: ").upper()
+def encrypt(msg):
 
     enc=""
 
@@ -18,11 +17,10 @@ def encrypt():
 
     return enc
 
-def decrypt():
-    enc = input("Enter the encoded message: ").upper()
+def decrypt(msg):
 
     dec=""
-    for i in enc:
+    for i in msg:
         if i.isalnum():
             k=0
             for j in key_table.values():
